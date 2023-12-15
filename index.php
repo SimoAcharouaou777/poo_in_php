2af08d374fdd77bf1c3d752a96f7ca9cbc5baa16
SimoAcharouaou777 <acharouaoumohamed@gmail.com>
2023-12-15 10:29:56 +0100

    working on autoload

--- index.php
+++ index.php
@@ -1,46 +1,35 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/style/index.css">;
+    <title>HOME</title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php">LOGO</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="vue/signup.php">Sign up</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="vue/login.php">Log in</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
-
-class Android{
-    public $ram;
-    public $space;
-    public $color;
-    public $inches;
-    public $ownername;
-    private $pass;
-    const OWNERNAME = 4;
-
-    public function changeProp($ra,$spa,$col,$inch,$owner){
-        $this->ram        = $ra;
-        $this->space      = $spa;
-        $this->color      = $col;
-        $this->inches     = $inch;
-        $this->ownername = $owner;
-        
-    }
-    public function changepass($newpass){
-        $this->pass = sha1($newpass) ;
-    }
-    public function checkowner(){
-        if(strlen($this->ownername) < self::OWNERNAME){
-            echo"the name must be more than ". self::OWNERNAME. "charachters";
-        }else{
-            echo"your name has been set";
-        }
-    }
-}
-
-$redmiNot7 = new Android();
-$redmiNot7->changeProp("8GB" , "256GB", "GOLDEN" , "6inches" , "Mohamed");
-$redmiNot7->checkowner();
-$redmiNot8 = new Android();
-$redmiNot8->changeProp("6GB" , "56GB", "SILVER" , "5inches" , "ABDELLALI");
-$redmiNot8->checkowner();
-$redmiNot8->changepass('simo@ach');
-
-
-echo '<pre>';
-var_dump($redmiNot7);
-var_dump($redmiNot8);
-echo '</pre>';
+</body>
+</html>
 
